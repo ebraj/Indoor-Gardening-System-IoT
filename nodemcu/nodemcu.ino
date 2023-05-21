b3ac2a6c315305d7f6437ca9ec5c10be9033d5f8
Ebraj Grg <gurungebraj22@gmail.com>
2023-05-21 23:45:12 +0545

    nodemcu code updated

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,6 +1,6 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID ""
-#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_TEMPLATE_ID "TMPLGmmGGs3i"
+#define BLYNK_TEMPLATE_NAME "dht11nodemcu"
 #define BLYNK_AUTH_TOKEN ""
 
 #include <ESP8266WiFi.h>
@@ -16,27 +16,36 @@ int previousHumidity = 5;
 int previousTemperature = 5;
 int previousLight = 5;
 int previousMoisture = 5;
+int systemSwitchVal = 1;
+int fanPin = 0;
+int lightPin = 0;
+int pumpPin = 0;
 
-BLYNK_WRITE(V4){
+BLYNK_WRITE(V4) {
   int pin = param.asInt();
-  digitalWrite(D2, pin);
+  systemSwitchVal = pin;
 }
-BLYNK_WRITE(V5){
+BLYNK_WRITE(V5) {
   int pin = param.asInt();
-  digitalWrite(D2, pin);
+  fanPin = pin;
 }
-BLYNK_WRITE(V6){
+BLYNK_WRITE(V6) {
   int pin = param.asInt();
-  digitalWrite(D3, pin);
+  lightPin = pin;
 }
+BLYNK_WRITE(V7) {
+  int pin = param.asInt();
+  pumpPin = pin;
+}
+
 void setup() {
   Serial.begin(9600);
-  pinMode(D2, OUTPUT);
+  pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   Blynk.begin(auth, ssid, pass);
 }
-void loop(){
+void loop() {
   Blynk.run();
   if (Serial.available() > 0) {
     StaticJsonDocument<200> jsonDocs;
@@ -46,53 +55,75 @@ void loop(){
     int temperatureData = jsonDocs["temperature"];
     int newLightData = jsonDocs["new_light"];
     int moistureData = jsonDocs["moisture"];
-
-
     if (humidityData != previousHumidity && humidityData > 0) {
-      Serial.println(humidityData);
       Blynk.virtualWrite(V0, humidityData);
       previousHumidity = humidityData;
     }
 
     if (temperatureData != previousTemperature && temperatureData > 0) {
-      Serial.println(temperatureData);
       Blynk.virtualWrite(V1, temperatureData);
       previousTemperature = temperatureData;
-      if(temperatureData < 50){
-        // Blynk.virtualWrite(V4, HIGH);
-        digitalWrite(D1, LOW);
-      }
-      else{
-        // Blynk.virtualWrite(V4, LOW);
-        digitalWrite(D1, HIGH);
-      }
     }
 
     if (newLightData != previousLight && newLightData > 0) {
-      Serial.println(newLightData);
       Blynk.virtualWrite(V2, newLightData);
       previousLight = newLightData;
-      if(newLightData < 8){
-        // Blynk.virtualWrite(V5, HIGH);
-        digitalWrite(D2, HIGH);
-      }
-      else{
-        // Blynk.virtualWrite(V5, LOW);
-        digitalWrite(D2, LOW);
-      }
     }
 
     if (moistureData != previousMoisture && moistureData > 0) {
-      Serial.println(moistureData);
       Blynk.virtualWrite(V3, moistureData);
       previousMoisture = moistureData;
-      if(moistureData < 50){
-        // Blynk.virtualWrite(V6, HIGH);
-        digitalWrite(D3, LOW);
+    }
+
+    if (systemSwitchVal == 0) {
+      // Automatic Control
+      Serial.println("I am inside automatic");
+      if (temperatureData > 28) {
+        digitalWrite(D1, LOW);
+      } else {
+        digitalWrite(D1, HIGH);
       }
-      else{
-        // Blynk.virtualWrite(V6, LOW);
+
+      if (newLightData < 15) {
+        digitalWrite(D2, HIGH);
+      } else {
+        digitalWrite(D2, LOW);
+      }
+
+      if (moistureData < 50) {
+        digitalWrite(D3, LOW);
+      } else {
         digitalWrite(D3, HIGH);
+      }
+    } else {
+      Serial.println("I am inside manual");
+      // Manual Control
+      if (fanPin == 0) {
+        digitalWrite(D1, HIGH);
+        Serial.println("Fan running.");
+        Blynk.logEvent("fan_running");
+      } else {
+        digitalWrite(D1, LOW);
+        Serial.println("Fan stopped.");
+        Blynk.logEvent("Fan stopped.");
+      }
+      if (lightPin == 0) {
+        digitalWrite(D2, HIGH);
+        Serial.println("Light running.");
+        Blynk.logEvent("Light running.");
+      } else {
+        digitalWrite(D2, LOW);
+        Serial.println("Light stopped.");
+        Blynk.logEvent("Light stopped.");
+      }
+      if (pumpPin == 0) {
+        digitalWrite(D3, HIGH);
+        Serial.println("Pump running.");
+        Blynk.logEvent("Pump running.");
+      } else {
+        digitalWrite(D3, LOW);
+        Serial.println("Pump stopped.");
+        Blynk.logEvent("Pump stopped.");
       }
     }
   }
